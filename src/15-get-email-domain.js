ca4584c578d90d06272bf7b14099134272beb609
nikitausaty <nikitausatiy@yandex.ru>
2021-04-09 01:19:44 +0300

    feat: add 15 task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,10 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const at = '@';
+  const domain = email.lastIndexOf(at);
+  return email.slice([domain + 1]);
 }
 
 module.exports = getEmailDomain;
